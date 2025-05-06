5d7d2a88dbc84c81bfcbb1203e54b52e818a1afd
Pheemmawich Cheenlert <pheemmawich@Pheemmawichs-MacBook-Air.local>
2025-05-06 23:30:00 +0700

    prescripto full frontend (admin)

---
+++ README.md
@@ -0,0 +1,10 @@
+1.npm create vite@latest
+2.npm install
+3.npm i axios react-router-dom react-toastify
+4.(change port number in vite.config.js to be different from frontend before)npm run dev
+5.delete App.css file
+6.rafce in App.js file
+7.clear index.css file
+8.change title in index.html
+
+9.tailwind install 10. remove <strictMode> from main.jsx 11. replace it with <BrowserRouter> 12. create folder structure in src folder likes pages components context
---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import Login from "./pages/login";
+import { ToastContainer, toast } from "react-toastify";
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllApointment from "./pages/Admin/AllApointment";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import { Route, Routes } from "react-router-dom";
+import { DoctorContext } from "./context/doctorContext";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
+import DoctorAppointment from "./pages/Doctor/DoctorAppointment";
+import DoctorProfile from "./pages/Doctor/DoctorProfile";
+
+const App = () => {
+  const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
+
+  return aToken || dToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          {/* Admin Route */}
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllApointment />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+
+          {/* Doctor Route */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-appointments" element={<DoctorAppointment />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
